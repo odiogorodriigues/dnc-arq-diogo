00d9e1597095e2d56a2feff257199f3ad2759ca5
Diogo Rodrigues <diogogamerpudim@gmail.com>
2025-10-12 13:22:16 -0300

    Componentes compartilhados - Banners

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Button from "../components/Button/Button"
 import Header from "../components/Header/Header"
 import Footer from "../components/Footer/Footer"
+import Banner from "../components/Banner/Banner"
 
 function Home () {
   return (
     <>
       <Header />
       <Button buttonStyle="primary"arrow>Olá!</Button>
+      <Banner title="Olá!" image="about.jpg" />
       <Footer />
     </>
   )
