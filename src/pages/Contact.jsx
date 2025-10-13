d5f2959a492cd2566fa9908a46d240c29b0b0ec2
Diogo Rodrigues <diogogamerpudim@gmail.com>
2025-10-13 18:56:18 -0300

    Inserindo componentes na página de contato

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,18 @@
+import AboutText from "../components/AboutText/AboutText"
+import Header from "../components/Header/Header"
+import Footer from "../components/Footer/Footer"
+import Banner from "../components/Banner/Banner"
+import ContactForm from "../components/ContactForm/ContactForm"
+
 function Contact () {
   return (
     <>
-      <h1>Contact</h1>
-      <p>Subtitle contact</p>
+      <Header />
+      <Banner title="Contact" image="contact.jpg" />
+      <div className="container">
+        <ContactForm />
+      </div>
+      <Footer />
     </>
   )
 }
