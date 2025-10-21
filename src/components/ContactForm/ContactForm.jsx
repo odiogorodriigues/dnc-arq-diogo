81683de3370c1d56f3e16eb3fba94d8895f4f89f
odiogorodriigues <diogogamerpudim@gmail.com>
2025-10-21 18:01:44 -0300

    Validação de formulário com Hooks

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,9 +1,48 @@
+import { useEffect, useState } from "react";
 import "./ContactForm.css";
 
 // COMPONENTS
 import Button from "../Button/Button";
 
 function ContactForm() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (isFormValid) {
+      null;
+    }
+  };
+
+  useEffect(() => {
+    const isValidEmail = (email) => {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(email);
+    };
+
+    const isValid =
+      formData.name.trim() &&
+      formData.email.trim() &&
+      isValidEmail(formData.email) &&
+      formData.message.trim();
+
+    setIsFormValid(isValid);
+  }, [formData]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+        ...formData,
+        [name]: value
+    })
+  }
+
   return (
     <div className="contact-form d-flex fd-column al-center">
       <h2>We love meeting new people and helping them.</h2>
@@ -15,6 +54,7 @@ function ContactForm() {
             id="name"
             name="name"
             placeholder="Name *"
+            onChange={handleChange}
           />
 
           <input
@@ -23,6 +63,7 @@ function ContactForm() {
             id="email"
             name="email"
             placeholder="Email *"
+            onChange={handleChange}
           />
         </div>
 
@@ -32,12 +73,13 @@ function ContactForm() {
             name="message"
             id="message"
             placeholder="Mensagem *"
+            onChange={handleChange}
             rows="4"
           ></textarea>
         </div>
 
         <div className="al-center d-flex jc-end form-group">
-          <Button type="submit" buttonStyle="secondary">
+          <Button type="submit" buttonStyle="secondary" disabled={!isFormValid}>
             Enviar
           </Button>
         </div>
