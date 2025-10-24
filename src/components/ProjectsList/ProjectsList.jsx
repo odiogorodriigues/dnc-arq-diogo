ba7a9fcff758f1801b938f49c977045f982327c9
odiogorodriigues <diogogamerpudim@gmail.com>
2025-10-24 10:47:05 -0300

    Usando Session Storage no projeto

--- src/components/ProjectsList/ProjectsList.jsx
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -1,9 +1,12 @@
 import { useContext, useEffect, useState } from "react";
 import "./ProjectsList.css";
 
-//ASSETS
+// ASSETS
 import LikedFilled from "../../assets/like-filled.svg";
-import Like from "../../assets/like.svg";
+import LikeOutline from "../../assets/like.svg";
+
+// COMPONENTS
+import Button from "../../components/Button/Button";
 
 // CONTEXT
 import { AppContext } from "../../contexts/AppContext";
@@ -12,48 +15,86 @@ import { AppContext } from "../../contexts/AppContext";
 import { getApiData } from "../../services/apiServices";
 
 function ProjectsList() {
-    const [projects, setProjects] = useState([]);
-    const appContext = useContext(AppContext);
+  const [projects, setProjects] = useState([]);
+  const [favProject, setFavProject] = useState([]);
+  const appContext = useContext(AppContext);
+  const handleSavedProjects = (id) => {
+    setFavProject((prevFavProjects) => {
+      if (prevFavProjects.includes(id)) {
+        const filterArray = prevFavProjects.filter(
+          (projectId) => projectId !== id
+        );
+        sessionStorage.setItem("favProjects", JSON.stringify(filterArray));
+        return prevFavProjects.filter((projectId) => projectId !== id);
+      } else {
+        sessionStorage.setItem(
+          "favProjects",
+          JSON.stringify([...prevFavProjects, id])
+        );
+        return [...prevFavProjects, id];
+      }
+    });
+  };
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const projectsResponse = await getApiData("projects");
-                setProjects(projectsResponse);
-            } catch {
-                setProjects([]);
-            }
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const projectsResponse = await getApiData("projects");
+        setProjects(projectsResponse);
+      } catch {
+        setProjects([]);
+      }
+    };
 
-        fetchData();
-    }, []);
-    
-    return (
-        <div className='projects-section'>
-            <div className='projects-hero'>
-                <h2>{appContext.languages[appContext.language].projects.title}</h2>
-                <p>{appContext.languages[appContext.language].projects.subtitle}</p>
-            </div>
-            <div className='projects-grid'>
-                {
-                    projects ? 
-                        projects.map((project) => (
-                            <div
-                                className='project-card d-flex jc-center al-center fd-column'
-                                key={project.id}
-                            >
-                                <div className='thumb tertiary-background' style={{ backgroundImage: `url(/${project.thumb})` }} ></div>
-                                <h1>{project.id}</h1>
-                                <h3>{project.title}</h3>
-                                <p>{project.subtitle}</p>
-                                <img src={LikedFilled} height='20px' />
-                            </div>
-                        )) 
-                    : null
-                }
-            </div> 
-        </div>
-    );
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const savedFavProjects = JSON.parse(sessionStorage.getItem('favProjects'))
+    if (savedFavProjects) {
+        setFavProject(savedFavProjects)
+    }
+  }, []);
+
+  return (
+    <div className="projects-section">
+      <div className="projects-hero">
+        <h2>{appContext.languages[appContext.language].projects.title}</h2>
+        <p>{appContext.languages[appContext.language].projects.subtitle}</p>
+      </div>
+      <div className="projects-grid">
+        {projects
+          ? projects.map((project) => (
+              <div
+                className="project-card d-flex jc-center al-center fd-column"
+                key={project.id}
+              >
+                <div
+                  className="thumb tertiary-background"
+                  style={{ backgroundImage: `url(/${project.thumb})` }}
+                ></div>
+                <h1>{project.id}</h1>
+                <h3>{project.title}</h3>
+                <p>{project.subtitle}</p>
+                <Button
+                  buttonStyle="unstyled"
+                  onClick={() => handleSavedProjects(project.id)}
+                >
+                  <img
+                    src={
+                      favProject.includes(project.id)
+                        ? LikedFilled
+                        : LikeOutline
+                    }
+                    height="20px"
+                  />
+                </Button>
+              </div>
+            ))
+          : null}
+      </div>
+    </div>
+  );
 }
 
 export default ProjectsList;
